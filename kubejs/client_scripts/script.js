b4b1da26afab5eac58bf7d29b9ae791cd9cbbc1b
Damodigy <43088109+Damodigy@users.noreply.github.com>
2023-07-24 22:01:50 +0700

    Fix by Clown

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -4,5 +4,10 @@ console.info('Hello, World! (You will see this line every time client resources 
 
 onEvent('jei.hide.items', event => {
 	// Hide items in JEI here
+	event.hide('immersiveengineering:blastbrick')
+	event.hide('immersiveengineering:cokebrick')
+	event.hide('immersiveengineering:alloybrick')
+	event.hide('immersiveengineering:blastbrick_reinforced')
 	// event.hide('minecraft:cobblestone')
 })
+
